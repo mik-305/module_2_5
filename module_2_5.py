c6d4cead02405b47446ce83616f975068127faf2
mik-305 <mik-305@bk.ru>
2024-08-09 15:23:33 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,17 @@
+def get_matrix(n, m, value):    # Cоздали функцию 
+    matrix = []
+    for i in range(n):
+        matrix.append([])       # Пустой список вставили в пуст список
+        for k in range(m):
+            matrix[i].append(value)
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
+
+
